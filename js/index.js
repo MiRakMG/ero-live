9c3967a91d00326d77133562b64c2619db0efe30
Mialy Rak <mialyr4k@gmail.com>
2025-11-07 05:20:40 +0300

    update : profile page
    Signed-off-by: Mialy Rak <mialyr4k@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,23 +1,27 @@
 (function () {
-  const btn = document.getElementById("menuBtn");
+  const btns = document.querySelectorAll(".menuBtn");
   const aside = document.getElementById("sidebar");
   const main = document.getElementById("main");
+  const ads1 = document.getElementById("ads-1");
   const body = document.body;
-  if (!btn || !aside || !main) return;
+  if (btns.length === 0 || !aside || !main) return;
 
-  btn.addEventListener("click", function () {
-    aside.classList.toggle("flex!");
-    main.classList.toggle("lg:ml-56");
-    const expanded = btn.getAttribute("aria-expanded") === "true";
-    btn.setAttribute("aria-expanded", String(!expanded));
+  btns.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      aside.classList.toggle("flex!");
+      main.classList.toggle("lg:ml-60");
+      const expanded = btn.getAttribute("aria-expanded") === "true";
+      btn.setAttribute("aria-expanded", String(!expanded));
+      ads1.classList.toggle("hidden!");
+    });
   });
   if (
     aside.classList.contains("flex!") &&
-    main.classList.contains("lg:ml-56")
+    main.classList.contains("lg:ml-60")
   ) {
     body.addEventListener("click", function (event) {
       aside.classList.remove("flex!");
-      main.classList.remove("lg:ml-56");
+      main.classList.remove("lg:ml-60");
     });
   }
 })();
